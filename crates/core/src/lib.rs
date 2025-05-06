c1797467b0cfa54d3ba1780afd030503fd2dc563
Luis-Varona <lbvarona@mta.ca>
2025-05-06 00:22:15 -0300

    WIP: Continue core subcrate

--- crates/core/src/lib.rs
+++ crates/core/src/lib.rs
@@ -2,6 +2,10 @@ mod config;
 mod crawl;
 mod document;
 mod error;
+mod pipeline;
 
-pub use document::Document;
-pub use error::{CoreError, CoreResult};
+pub use config::*;
+pub use crawl::*;
+pub use document::*;
+pub use error::*;
+pub use pipeline::*;
--- crates/core/src/pipeline.rs
+++ crates/core/src/pipeline.rs
@@ -1,1 +1,7 @@
+use crate::*;
+use shadowseek_extractor::*;
+use shadowseek_hashing::*;
+use shadowseek_index::*;
+use shadowseek_tui::*;
+
 // TODO: Implement
