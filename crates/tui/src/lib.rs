e7761738d2b654cfe73a0b728c787e2152481cac
Luis-Varona <lbvarona@mta.ca>
2025-05-10 21:12:46 -0300

    Add licensing/copyright headers to all Rust files

--- crates/tui/src/lib.rs
+++ crates/tui/src/lib.rs
@@ -1,14 +1,11 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+// Copyright 2025 Luis M. B. Varona
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
+// http://opensource.org/licenses/MIT>, at your option. This file may not be
+// copied, modified, or distributed except according to those terms.
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+mod ui;
+mod widgets;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+// TODO: Implement
